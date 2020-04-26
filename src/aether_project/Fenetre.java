1801d11ced624c018a65116889b463105ba4d86a
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-26 14:15:42 +0100

    Carcle qui ricoche

--- src/aether_project/Fenetre.java
+++ src/aether_project/Fenetre.java
@@ -19,7 +19,6 @@ public class Fenetre extends JFrame {
 //		this.setContentPane(pan);
 //		this.setVisible(true);
 //	}
-	
 
 //	public Fenetre() {
 //		this.setTitle("Ma première fenêtre Java");
@@ -29,7 +28,6 @@ public class Fenetre extends JFrame {
 //		this.setContentPane(new Panneau());
 //		this.setVisible(true);
 //	} 
-	
 
 	private Panneau pan = new Panneau();
 
@@ -44,23 +42,79 @@ public class Fenetre extends JFrame {
 	}
 
 	private void go() {
-		for (int i = -50; i < pan.getWidth(); i++) {
-			
-			int x = pan.getPosX();
-			int y = pan.getPosY();
-			
-			x++;
-			y++;
-			
-			pan.setPosX(x);
-			pan.setPosY(y);
-			
+//		for (;;) {
+//			int x = pan.getPosX(), y = pan.getPosY();
+//			x++;
+//			y++;
+//			
+//			System.out.println("PosX : " + x + ", PosY : " + y);
+//			
+//			pan.setPosX(x);
+//			pan.setPosY(y);
+//			pan.repaint();
+//			try {
+//				Thread.sleep(10);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			// Si nos coordonnées arrivent au bord de notre composant
+//			// On réinitialise
+//			if (x == pan.getWidth() || y == pan.getHeight()) {
+//				pan.setPosX(-50);
+//				pan.setPosY(-50);
+//			}
+//		}
+		
+		// Les coordonnées de départ de notre rond
+		int x = pan.getPosX(), y = pan.getPosY();
+		// Le booléen pour savoir si l'on recule ou non sur l'axe x
+		boolean backX = false;
+		// Le booléen pour savoir si l'on recule ou non sur l'axe y
+		boolean backY = false;
+
+		// Dans cet exemple, j'utilise une boucle while
+		// Vous verrez qu'elle fonctionne très bien
+		while (true) {
+			// Si la coordonnée x est inférieure à 1, on avance
+			if (x < 1)
+				backX = false;
+
+			// Si la coordonnée x est supérieure à la taille du Panneau moins la taille du
+			// rond, on recule
+			if (x > pan.getWidth() - 50)
+				backX = true;
+
+			// Idem pour l'axe y
+			if (y < 1)
+				backY = false;
+			if (y > pan.getHeight() - 50)
+				backY = true;
+
+			// Si on avance, on incrémente la coordonnée
+			// backX est un booléen, donc !backX revient à écrire
+			// if (backX == false)
+			if (!backX)
+				pan.setPosX(++x);
+
+			// Sinon, on décrémente
+			else
+				pan.setPosX(--x);
+
+			// Idem pour l'axe Y
+			if (!backY)
+				pan.setPosY(++y);
+			else
+				pan.setPosY(--y);
+
+			// On redessine notre Panneau
 			pan.repaint();
+
+			// Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
 			try {
-				Thread.sleep(10);
+				Thread.sleep(3);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-	}     
+	}
 }
--- src/aether_project/Panneau.java
+++ src/aether_project/Panneau.java
@@ -11,11 +11,43 @@ public class Panneau extends JPanel {
 //		System.out.println("Je suis exécutée !");
 //		g.fillOval(20, 20, 75, 75);
 
+//	private int posX = -50;
+//	private int posY = -50;
+//
+//	public void paintComponent(Graphics g) {
+//		g.setColor(Color.red);
+//		g.fillOval(posX, posY, 50, 50);
+//	}
+//
+//	public int getPosX() {
+//		return posX;
+//	}
+//
+//	public void setPosX(int posX) {
+//		this.posX = posX;
+//	}
+//
+//	public int getPosY() {
+//		return posY;
+//	}
+//
+//	public void setPosY(int posY) {
+//		this.posY = posY;
+//	}
+
+//	}
+
 	private int posX = -50;
 	private int posY = -50;
 
 	public void paintComponent(Graphics g) {
+		// On choisit une couleur de fond pour le rectangle
+		g.setColor(Color.white);
+		// On le dessine de sorte qu'il occupe toute la surface
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		// On redéfinit une couleur pour le rond
 		g.setColor(Color.red);
+		// On le dessine aux coordonnées souhaitées
 		g.fillOval(posX, posY, 50, 50);
 	}
 
@@ -34,5 +66,4 @@ public class Panneau extends JPanel {
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
-//	}
 }
