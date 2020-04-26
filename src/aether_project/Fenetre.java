54bae349712fd19a46343dc62aa541454828d321
MokoMaracucho <desmoulin.federico@gmail.com>
2020-04-26 13:58:12 +0100

    Translation cercle

--- src/aether_project/Fenetre.java
+++ src/aether_project/Fenetre.java
@@ -21,12 +21,46 @@ public class Fenetre extends JFrame {
 //	}
 	
 
+//	public Fenetre() {
+//		this.setTitle("Ma première fenêtre Java");
+//		this.setSize(100, 150);
+//		this.setLocationRelativeTo(null);
+//		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		this.setContentPane(new Panneau());
+//		this.setVisible(true);
+//	} 
+	
+
+	private Panneau pan = new Panneau();
+
 	public Fenetre() {
-		this.setTitle("Ma première fenêtre Java");
-		this.setSize(100, 150);
-		this.setLocationRelativeTo(null);
+		this.setTitle("Animation");
+		this.setSize(300, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setContentPane(new Panneau());
+		this.setLocationRelativeTo(null);
+		this.setContentPane(pan);
 		this.setVisible(true);
-	} 
+		go();
+	}
+
+	private void go() {
+		for (int i = -50; i < pan.getWidth(); i++) {
+			
+			int x = pan.getPosX();
+			int y = pan.getPosY();
+			
+			x++;
+			y++;
+			
+			pan.setPosX(x);
+			pan.setPosY(y);
+			
+			pan.repaint();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}     
 }
--- src/aether_project/Panneau.java
+++ src/aether_project/Panneau.java
@@ -1,13 +1,38 @@
 package aether_project;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class Panneau extends JPanel {
+//	public void paintComponent(Graphics g) {
+	// Vous verrez cette phrase chaque fois que la méthode sera invoquée
+//		System.out.println("Je suis exécutée !");
+//		g.fillOval(20, 20, 75, 75);
+
+	private int posX = -50;
+	private int posY = -50;
+
 	public void paintComponent(Graphics g) {
-		// Vous verrez cette phrase chaque fois que la méthode sera invoquée
-		System.out.println("Je suis exécutée !");
-		g.fillOval(20, 20, 75, 75);
+		g.setColor(Color.red);
+		g.fillOval(posX, posY, 50, 50);
 	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+//	}
 }
